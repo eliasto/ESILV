77e4964c9440844a7399a754cc8c789a6dadfc2b
TOURNEUX Elias <55650958+eliasto@users.noreply.github.com>
2021-03-15 17:12:53 +0100

    TD 6 sans exo 7
    TD 6 complet sans l'exo 7 (normalement ça fonctionne, enfin j'espère)

--- TD Matrice 6/TD Matrice 6/Program.cs
+++ TD Matrice 6/TD Matrice 6/Program.cs
@@ -6,7 +6,7 @@ namespace TD_Matrice_6
     {
         static void Main(string[] args)
         {
-            exo4();
+            exo7();
         }
         static void AfficherMatrice(int[,] matrice)
         {
@@ -37,6 +37,42 @@ namespace TD_Matrice_6
             }
             
         }
+        static void AfficherTableau(double[] tab)
+        {
+            if (tab != null)
+            {
+                int compteur = 0;
+                if (tab == null)
+                {
+                    Console.WriteLine("tableau null");
+                    return;
+                }
+                foreach (int k in tab)
+                {
+                    compteur = k + compteur;
+                }
+                if (compteur == 0)
+                {
+                    Console.WriteLine("tableau vide");
+                }
+                if (compteur != 0 && tab != null)
+                {
+                    for (int i = 0; i < tab.Length; i++)
+                    {
+                        if (i == tab.Length - 1)
+                        {
+                            Console.Write(tab[i] + "");
+                        }
+                        else
+                        {
+                            Console.Write(tab[i] + "; ");
+                        }
+                    }
+                }
+            }
+
+        }
+
 
         static void exo1()
         {
@@ -158,6 +194,81 @@ namespace TD_Matrice_6
 
             AfficherMatrice(AdditionnerMatrices(matrice1, matrice2));
 
+        }
+        static void exo5()
+        {
+            double[] calcul_moyenne_matiere(int[,] matrice)
+            {
+                double[] tab = new double[matrice.GetLength(1)];
+                if (matrice != null && matrice.Length != 0)
+                {
+                    for(int i = 0; i < matrice.GetLength(1); i++)
+                    {
+                        double compteur = 0;
+                        for(int j = 0; j < matrice.GetLength(0); j++)
+                        {
+                            compteur = compteur + matrice[j, i];
+                        }
+                        tab[i] = compteur / matrice.GetLength(0);
+                    }
+                    return tab;
+                } else
+                {
+                    return null;
+                }
+            }
+            int[,] matrice = { { 1, 2, 3, 4 }, { 5, -6, 7, 8 }, { 9, 10, 11, 12 } };
+            AfficherTableau(calcul_moyenne_matiere(matrice));
+
+        }
+        static void exo6()
+        {
+            int[,] MatriceTransposee(int[,] matrice)
+            {
+                if (matrice != null && matrice.Length != 0 && matrice.GetLength(0) == matrice.GetLength(1))
+                {
+                    int[,] mat = new int[matrice.GetLength(0), matrice.GetLength(1)];
+
+                    for(int i = 0; i < matrice.GetLength(0); i++)
+                    {
+                        for(int j = 0; j < matrice.GetLength(1); j++)
+                        {
+                            mat[i, j] = matrice[j, i];
+                        }
+                    }
+                    return mat;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            int[,] matrice = { { 1, 2, 3, 4 }, { 5, -6, 7, 8 }, { 9, 10, 11, 12 },{ 2, 5, 7, 1 } };
+            AfficherMatrice(MatriceTransposee(matrice));
+        }
+        static void exo7()
+        {
+            int[,] ProduitMatriciel(int[,]m1,int[,]m2)
+            {
+                if (m1 != null && m1.Length != 0 &&m2!=null&&m2.Length!=0)
+                {
+                    int[,] matrice = new int[m1.GetLength(0), m1.GetLength(1)];
+                    for(int i = 0; i < m1.GetLength(0); i++)
+                    {
+                        for(int j = 0; j < m1.GetLength(1); j++)
+                        {
+
+                        }
+                    }
+                    return matrice;
+                }else
+                {
+                    return null;
+                }
+                }
+            int[,] m2 = { { 9, 10, 11, 12 }, { 5, -6, 7, 8 }, { 9, 10, 11, 12 } };
+            int[,] m1 = { { 5, -6, 7, 8 }, { 5, -6, 7, 8 }, { 9, 10, 11, 12 } };
+            AfficherMatrice(ProduitMatriciel(m1, m2));
         }
 
     }
