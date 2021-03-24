ccde42c755d7129efc0bcac4ee3a710dd770c4a2
TOURNEUX Elias <55650958+eliasto@users.noreply.github.com>
2021-03-24 16:36:40 +0100

    TD 8
    Mise en ligne du TD 8

---
+++ TD 7 Matrice/TD 7 Matrice/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace TD_7_Matrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            exo3();
+        }
+        static void exo1()
+        {
+            bool Permuter(int[] tableau, int index1, int index2)
+            {
+                bool boolean = false;
+                if (tableau != null && tableau.Length != 0 && tableau.Length-1 >= index1 && tableau.Length-1 >= index2 && !(index1>tableau.Length-1) && !(index2>tableau.Length-1))
+                {
+                    boolean = true;
+                    int a = tableau[index1];
+                    int b = tableau[index2];
+
+                    tableau[index1] = b;
+                    tableau[index2] = a;
+                }
+                foreach (int i in tableau)
+                {
+                    Console.Write(i+"; ");
+                }
+
+                return boolean;
+            }
+
+            int[] tab = new int[] { 1, 2, 3 };
+            Console.WriteLine(Permuter(tab,1,2));
+            
+        }
+        static void exo2()
+        {
+            int[] tab = { 3, 1, 10, 4, 56, 4, 3,29 };
+            int[] TriBulles(int[] tab)
+            {
+                if(tab!=null && tab.Length != 0)
+                {
+                    int compteur = 1;
+                    while (compteur != 0)
+                    {
+                        compteur = 0;
+                        for (int i = 0; i < tab.Length-1; i++)
+                        {
+                            if (tab[i] > tab[i + 1])
+                            {
+                                compteur++;
+                                int a = tab[i];
+                                int b = tab[i + 1];
+
+                                tab[i] = b;
+                                tab[i + 1] = a;
+                            }
+                        }
+                       
+                    }
+                }
+
+                return tab;
+            }
+
+            Console.WriteLine("");
+            foreach(int i in tab)
+            {
+                Console.Write(i + ";");
+            }
+            Console.WriteLine("");
+
+            foreach (int i in TriBulles(tab))
+            {
+                Console.Write(i + ";");
+            }
+
+        }
+        static void exo3()
+        {
+            int[] tab = { 3, 1, 10, 4, 56, 4, 3, 29 };
+            int[] TriBulles(int[] tab)
+            {
+                if (tab != null && tab.Length != 0)
+                {
+                    
+                    int compteur = 1;
+                    int etat = 0;
+                    while (compteur != 0)
+                    {
+                       
+                        compteur = 0;
+                        if(etat == 0)
+                        {
+                            etat++;
+                            for (int i = 0; i < tab.Length - 1; i++)
+                            {
+                                if (tab[i] > tab[i + 1])
+                                {
+                                    compteur++;
+                                    int a = tab[i];
+                                    int b = tab[i + 1];
+
+                                    tab[i] = b;
+                                    tab[i + 1] = a;
+                                }
+                            }
+                        } else
+                        {
+                            etat = 0;
+                            for (int i = 1; i < tab.Length; i++)
+                            {
+                                if (tab[i] < tab[i - 1])
+                                {
+                                    compteur++;
+                                    int a = tab[i];
+                                    int b = tab[i - 1];
+
+                                    tab[i] = b;
+                                    tab[i - 1] = a;
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+                return tab;
+            }
+
+            Console.WriteLine("");
+            foreach (int i in tab)
+            {
+                Console.Write(i + ";");
+            }
+            Console.WriteLine("");
+
+            foreach (int i in TriBulles(tab))
+            {
+                Console.Write(i + ";");
+            }
+        }
+    }
+}
---
+++ TD 8 MATRICE/TD 8 MATRICE/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace TD_8_MATRICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            exo3();
+        }
+        static void exo1()
+        {
+            int[] TriTableau(int[] tab)
+            {
+                if(tab!=null && tab.Length != 0)
+                {
+
+                    for (int i = 0; i < tab.Length - 1; i++)
+                    {
+                        for (int j = i + 1; j > 0; j--)
+                        {
+                            if (tab[j - 1] > tab[j])
+                            {
+                                int valeur = tab[j - 1];
+                                tab[j - 1] = tab[j];
+                                tab[j] = valeur;
+                            }
+                        }
+                    }
+
+                }
+                return tab;
+            }
+
+            int[] tab = { 1, 2, 3, 4, 6, 5, 9, 4, 2, 7, 13, 22, 3 };
+            tab = TriTableau(tab);
+            foreach(int i in tab)
+            {
+                Console.Write(i + ";");
+            }
+        }
+        static void exo2()
+        {
+            int[] Melange(int[] tab)
+            {
+                if (tab != null & tab.Length != 0)
+                {
+                    Random random = new Random();
+                    while(random.Next(0,100)!=3)
+                    {
+                        int aleat1 = random.Next(0, tab.Length);
+                        int aleat2 = random.Next(0, tab.Length);
+                        int a = tab[aleat1];
+                        int b = tab[aleat2];
+
+                        tab[aleat1] = b;
+                        tab[aleat2] = a;
+                    }
+
+                }
+                return tab;
+            }
+
+            int[] tab = { 1, 2, 3, 4, 6, 5, 9, 4, 2, 7, 13, 22, 3 };
+            tab = Melange(tab);
+            foreach(int i in tab)
+            {
+                Console.Write(i + ";");
+            }
+        }
+        static void exo3()
+        {
+            int[] TriSelect(int[] tab)
+            {
+                if (tab != null & tab.Length != 0)
+                {
+                    int little;
+                    int compteur = 0;
+                    for(int i = 0; i < tab.Length; i++)
+                    {
+                        little = tab[i];
+                        for(int j = i; j < tab.Length; j++)
+                        {
+                            if (tab[j] < little)
+                            {
+                                little = tab[j];
+                                tab[j] = tab[compteur];
+                                tab[compteur] = little;
+
+                            }
+                        }
+                        compteur++;
+                    }
+                }
+                return tab;
+            }
+
+            int[] tab = { 1, 2, 3, 4, 6, 5, 9, -3, 2, 7, 13, 22, 3 };
+            tab = TriSelect(tab);
+            foreach(int i in tab)
+            {
+                Console.Write(i + ";");
+            }
+        }
+    }
+}
